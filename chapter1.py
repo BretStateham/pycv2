cbef86cbbf1db7dc48b58e69ff0063d5fbb03a3a
Bret Stateham <Bret.Stateham@microsoft.com>
2021-06-01 18:15:10 -0700

    Image filters

--- chapter1.py
+++ chapter1.py
@@ -1,18 +1,25 @@
 import cv2
+import numpy as np
 
-print("Package Imported")
+img = cv2.imread("Resources/lena.png")
 
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-cap.set(10,100)
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+imgCanny = cv2.Canny(img,150,200)
 
 
-while True:
-    success, img = cap.read()
-    cv2.imshow("Video",img)
-    if cv2.waitKey(30) & 0xFF == ord('q'):
-        break
+kernel = np.ones((3,3), np.uint8)
 
+imgDialation = cv2.dilate(imgCanny,kernel, iterations=1)
+
+imgEroded = cv2.erode(imgDialation,kernel, iterations=1)
+
+cv2.imshow("Grayscale",imgGray)
+cv2.imshow("Blur",imgBlur)
+cv2.imshow("Canny",imgCanny)
+cv2.imshow("Dialation",imgDialation)
+cv2.imshow("Eroded",imgEroded)
+
+cv2.waitKey(0)
 
 
